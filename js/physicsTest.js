a4c4ef1fd33b6435d766505e308f9cf314f28307
TheDan64 <ThaDan64@gmail.com>
2014-01-26 15:47:47 -0500

    Added basic test files and dependencies.

---
+++ js/physicsTest.js
@@ -0,0 +1,10 @@
+/*
+	This is a test script by Daniel Kolsoi to test the box2d.js physics engine.
+	Created on 01/26/2014. Last edited on 01/26/2014.
+*/
+
+$(document).ready(function(){
+	
+
+
+});
