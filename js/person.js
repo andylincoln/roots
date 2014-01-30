c949dc4ca966697a81223312e08796fbe82c6306
Andrew Lincoln <andrewlincoln11@gmail.com>
2014-01-29 22:28:02 -0500

    person.js names in working state
    Removed the multiple constructors, (JavaScript does not support
    function overloading)
    
    Instead of the (first, middle last) order, it is now (first last
    middle), because middle is optional and defaults to undefined.

--- js/person.js
+++ js/person.js
@@ -9,68 +9,54 @@
  * Person Constructor
  * 
  * @param {String} firstName
- * @returns {Person}
- */
-function Person(firstName) {
-    Person(firstName, "", "");
-}
-
-/**
- * Person Constructor
- * 
- * @param {String} firstName ; not Null
- * @param {String} lastName  ; not Null
- * @returns {Person}
- */
-function Person(firstName, lastName) {
-    Person(firstName, "", lastName);
-}
-
-/**
- * Person Constructor
- * 
- * @param {String} firstName
- * @param {String} middleName
  * @param {String} lastName
+ * @param {String} middleName (Optional)
  * @returns {Person}
  */
 
-function Person(firstName, middleName, lastName) {
+function Person(firstName, lastName, middleName) {
 
+    this.dateBirth = new Date(0,1,1);
+    this.dateBirth.setFullYear(0000);
+    this.dateDeath = new Date(0,1,1);
+    this.dateDeath.setFullYear(0000);
     this.firstName = firstName;
-    this.middleName = middleName;
+    this.gender = "";
     this.lastName = lastName;
+    this.middleName = middleName;
 
-    /**
-     * Getter Definitions
-     */
+    //  Getter Definitions
+    this.getDateOfBirth = function() {
+        return this.dateBirth;
+    };
+    this.getDateOfDeath = function() {
+        return this.dateDeath;
+    };
     this.getFirstName = function() {
         return this.firstName;
     };
-    this.getMiddleName = function() {
-        return this.middleName;
+    this.getGender = function() {
+        return this.gender;
     };
     this.getLastName = function() {
         return this.lastName;
+    };  
+    this.getMiddleName = function() {
+        return this.middleName;
     };
     this.getName = function() {
-        
-        if (!this.getMiddleName() && !this.getLastName()) {
-            return this.getFirstName();
+        if (this.getMiddleName() === undefined) {
+            return (this.getFirstName() + " " + this.getLastName());
         }
-        if(!this.getMiddleName() && this.getLastName()) {
-            return (this.getFirstName() + "" + this.getMiddleName + "" + this.getLastName());
-        } 
-        
-        return (this.getFirstName() + "" + this.getMiddleName + "" + this.getLastName());
+        return (this.getFirstName() + " " + this.getMiddleName() + " " + this.getLastName());
     };
 
-    /**
-     * Setter Definitions
-     */
-    
+    //  Setter Definitions 
     this.setFirstName = function(newName) {
         this.firstName = newName;
+    };
+    this.setGender = function(gender) {
+        this.gender = gender;
     };
     this.setMiddleName = function(newName) {
         this.MiddleName = newName;
@@ -78,12 +64,6 @@ function Person(firstName, middleName, lastName) {
     this.setLastName = function(newName) {
         this.LastName = newName;
     };
-
-    /*
-     * gender
-     * date of birth
-     * date of death  
-     */
-
+    
 }
 
