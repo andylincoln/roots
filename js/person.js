043bd0ed6465a324d7da449d9062a37ef19e7716
Andrew Lincoln <andrewlincoln11@gmail.com>
2014-02-09 20:47:02 -0500

    Person object updates
    Added:
    Location object for use with residence & birthplace fields
    Spouse field
    Children field, represented by a Map
    
    Modified:
    non-name information defaults to null instead of garbage data

--- js/person.js
+++ js/person.js
@@ -16,16 +16,36 @@
 
 function Person(firstName, lastName, middleName) {
 
-    this.dateBirth = new Date(0,1,1);
-    this.dateBirth.setFullYear(0000);
-    this.dateDeath = new Date(0,1,1);
-    this.dateDeath.setFullYear(0000);
+    if (!(typeof (firstName) === "string")) {
+        throw Exception("Argument city to Location must be a String");
+    }
+    if (!(typeof (lastName) === "string")) {
+        throw Exception("Argument state to Location must be a String");
+    }
+    if (!(typeof (middleName) === "string")) {
+        throw Exception("Argument country to Location must be a String");
+    }
+
+    this.birthplace = null;
+    this.children = null;
+    this.dateBirth = null;
+    this.dateDeath = null;
     this.firstName = firstName;
-    this.gender = "";
+    this.id = Date.now();
+    this.gender = null;
+    this.generation = 1;
     this.lastName = lastName;
     this.middleName = middleName;
+    this.residence = null;
+    this.spouse = null;
 
     //  Getter Definitions
+    this.getBirthplace = function() {
+        return this.birthplace;
+    };
+    this.getChildren = function() {
+        return this.children;
+    };
     this.getDateOfBirth = function() {
         return this.dateBirth;
     };
@@ -38,9 +58,15 @@ function Person(firstName, lastName, middleName) {
     this.getGender = function() {
         return this.gender;
     };
+    this.getGeneration = function() {
+        throw Exception("Not yet implemneted");
+    };
+    this.getID = function() {
+        return this.id;
+    };
     this.getLastName = function() {
         return this.lastName;
-    };  
+    };
     this.getMiddleName = function() {
         return this.middleName;
     };
@@ -50,20 +76,94 @@ function Person(firstName, lastName, middleName) {
         }
         return (this.getFirstName() + " " + this.getMiddleName() + " " + this.getLastName());
     };
+    this.getResidence = function() {
+        return this.residence;
+    };
+    this.getSpouse = function() {
+        return this.spouse;
+    };
 
     //  Setter Definitions 
+
+    this.addChild = function(child) {
+
+        if (this.children === null) {
+            this.children = new Map();
+        }
+        this.children.set(child.getID(), child);
+    };
+    this.setBirthplace = function(location) {
+        this.birthplace = location;
+    };
+    this.setDateOfBirth = function(date) {
+        this.dateBirth = date;
+    };
     this.setFirstName = function(newName) {
         this.firstName = newName;
     };
     this.setGender = function(gender) {
         this.gender = gender;
     };
-    this.setMiddleName = function(newName) {
-        this.MiddleName = newName;
+    this.setGeneration = function(num) {
+        //TODO: Implement Generation counter
+        throw Exception("Not yet implemented");
     };
     this.setLastName = function(newName) {
         this.LastName = newName;
     };
-    
+    this.setMiddleName = function(newName) {
+        this.MiddleName = newName;
+    };
+    this.setResidence = function(location) {
+        this.residence = location;
+    };
+    this.setSpouse = function(spouse) {
+        this.spouse = spouse;
+    };
+
+}
+
+
+/**
+ * Location Constructor
+ * 
+ * @param {String} city
+ * @param {String} state
+ * @param {String} country
+ * @returns {Location}
+ */
+function Location(city, state, country) {
+
+    if (!(typeof (city) === "string")) {
+        throw Exception("Argument city to Location must be a String");
+    }
+    if (!(typeof (state) === "string")) {
+        throw Exception("Argument state to Location must be a String");
+    }
+    if (!(typeof (country) === "string")) {
+        throw Exception("Argument country to Location must be a String");
+    }
+
+    this.city = city;
+    this.state = state;
+    this.country = country;
+
+    this.getCity = function() {
+        return this.city;
+    };
+    this.getCountry = function() {
+        return this.country;
+    };
+    this.getState = function() {
+        return this.state;
+    };
+
+
+    // TODO: setters? Yes/No
+    /* Reasoning: No
+     *  A change to the location could just mean a new Location obj
+     *  Locations can also be made on the fly, and less likely to be fiddled
+     *  with accidentally
+     */
 }
 
