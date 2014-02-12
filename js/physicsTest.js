86ff2243784af605be1ae629e591d20e6841bc16
U-STUDENT\Daniel_Kolsoi <Daniel_Kolsoi@LYD-REF-2.fs.uml.edu>
2014-02-12 12:41:31 -0500

    Added css file for physic demo, included my own css & jQuery library in html.

--- js/physicsTest.js
+++ js/physicsTest.js
@@ -1,9 +1,11 @@
 /*
-	This is a test script by Daniel Kolsoi to test the box2d.js physics engine.
-	Created on 01/26/2014. Last edited on 01/26/2014.
+    Created on  : Jan 26 2014
+    Author      : Daniel Kolsoi
+    Description : Test script for the physics demo.
 */
 
 $(document).ready(function(){
 	var world = new b2World( new b2Vec2(0.0, -10.0) );
+	var canvas = document.getElementById('canvas');
 
 });
