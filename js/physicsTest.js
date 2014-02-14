d34f45e2a2f2c68176e4357a233cba8c666b0839
TheDan64 <ThaDan64@gmail.com>
2014-02-13 21:33:02 -0500

    Continued fleshing out the canvas demo.

--- js/physicsTest.js
+++ js/physicsTest.js
@@ -5,7 +5,7 @@
 */
 
 function Node() {
-	// stuff
+	var person = "Person";// = Person("Person");	
 
 	return {
 		// more stuff
@@ -14,40 +14,80 @@ function Node() {
 
 function Canvas(id) {
 	// Load data required to manipulate the canvas:
-	var cElement = document.getElementById(id);
-	var context  = cElement.getContext("2d");
+	var cElement = document.getElementById(id),
+		context = cElement.getContext("2d"),
+		scroll = {x: 0, y: 0},
+		tileSize = 80,
+		jID = '#' + id,
+		tiles = {x: 0, y: 0},
+		nodes = {};
 
-	function privateDraw() {
+	// This is the function that will draw to the canvas each frame:
+	function draw() {
+		// Start by clearing the screen:
+		context.clearRect(0, 0, cElement.width, cElement.height);
 
+		// Draw nodes and connections:
+
+
+
+		context.stroke();
 	}
 
-	function privateScroll(velocity) {
-		// Up / Down only scroll?
+	function scroll(xVelocity, yVelocity) {
+		// not final nor essential to demo:
+		scroll.x += xVelocity;
+		scroll.y += yVelocity;
+	}
+
+	function resetCamera() {
+		// Not sure if actually needed.
+		scroll.x = 0;
+		scroll.y = 0;
 	}
 
 	// Sets the width & height of the canvas:
-	function privateResize(width, height) {
+	function resize(width, height) {
 		context.canvas.width  = width;
 		context.canvas.height = height;
+
+		tiles.x = Math.ceil(width / tileSize);
+		tiles.y = Math.ceil(height/ tileSize);
 	}
 
 	// This function will handle the drawing and fps syncing:
-	function privateAnimate() {
+	function animate() {
 		var time = 0,
-			inc  = 0.1;
+			inc = 0.1;
 
 		// Immediate Anonymous Loop:
 		(function loop() {
 			requestAnimationFrame(loop);
 			time += inc;
-			// draw();
+			draw();
 		}());
 	}
 
+	// Handle the canvas being clicked:
+	 $(jID).click(function(e){
+	 	// x & y based on code from https://stackoverflow.com/questions/3067691/html5-canvas-click-event:
+	    var x = Math.floor((e.pageX-$(jID).offset().left) / tileSize);
+	    var y = Math.floor((e.pageY-$(jID).offset().top)  / tileSize);
+
+	    // Create a node if a blank tile clicked?
+	    if (true) { // figure out way to map nodes, check if not occupied already.
+	    	var node = Node();
+	    }
+
+	    // Needs to be moved out of this function:
+    	context.fillStyle = "rgb(255,0,0)";
+    	context.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
+ 	});
+
 	// Here is the returned JSOL which allows public access of certain functions:
 	return {
-		animate: privateAnimate,
-		resize:  privateResize
+		animate: animate,
+		resize: resize
 		// more stuff
 	};
 }
@@ -58,7 +98,9 @@ $(document).ready(function(){
 	var canvas = Canvas("canvas");
 	canvas.animate();
 
-	// Physics engine, probably scrapping
-	//var world = new b2World( new b2Vec2(0.0, -10.0) );
-
+	// tmp, for the demo. Although we will use similar logic:
+	canvas.resize(window.innerWidth, window.innerHeight);
+	$(window).resize(function() {
+		canvas.resize(window.innerWidth, window.innerHeight);
+	});
 });
