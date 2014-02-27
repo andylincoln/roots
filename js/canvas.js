768d2aa507ffeb021786bcbf5dfa961825a1556e
TheDan64 <ThaDan64@gmail.com>
2014-02-26 23:05:11 -0500

    changed canvas variable name and canvas class name to Andy's coding standards.

--- js/canvas.js
+++ js/canvas.js
@@ -1,200 +1,200 @@
 /*
-	Created on  : Jan 26 2014
-	Author      : Daniel Kolsoi
-	Description : Script for the canvas functions.
+    Created on  : Jan 26 2014
+    Author      : Daniel Kolsoi
+    Description : Script for the canvas functions.
 */
 
 function Node(x, y) {
-	// Data structure variables:
-	var pos = {x: x, y: y};
-	var person = Person();
+    // Data structure variables:
+    var pos = {x: x, y: y};
+    var person = Person();
 
-	//tmp:
-	person.setFirstName("Test");
-	person.setLastName("Name");
+    //tmp:
+    person.setFirstName("Test");
+    person.setLastName("Name");
 
-	// Getter function(s):
-	function getPosition() {
-		return pos;
-	}
+    // Getter function(s):
+    function getPosition() {
+        return pos;
+    }
 
-	function getData() {
-		return person;
-	}
+    function getData() {
+        return person;
+    }
 
-	return {
-		getData: getData,
-		getPosition: getPosition
-	};
+    return {
+        getData: getData,
+        getPosition: getPosition
+    };
 }
 
-function Canvas(id) {
-	// Canvas and buffer variables:
-	var mElement = document.getElementById(id),
-		bElement = document.createElement('canvas'),
-		mainBuffer = mElement.getContext("2d"),
-		backBuffer = bElement.getContext("2d");
+function Workspace(id) {
+    // Canvas and buffer variables:
+    var mElement = document.getElementById(id),
+        bElement = document.createElement('canvas'),
+        mainBuffer = mElement.getContext("2d"),
+        backBuffer = bElement.getContext("2d");
 
-	// Flag variables:
-	var redrawBuffer = false,
-		scrolling = false;
+    // Flag variables:
+    var redrawBuffer = false,
+        scrolling = false;
 
-	// Data Structure variables:
-	var scroll = {x: 0, y: 0},
-		selections = {left: null, right: null},
-		nodes = [];
+    // Data Structure variables:
+    var scroll = {x: 0, y: 0},
+        selections = {left: null, right: null},
+        nodes = [];
 
-	// This is the function that will draw to the back buffer only when something new happens on screen:
-	function draw() {
-		var pos;
+    // This is the function that will draw to the back buffer only when something new happens on screen:
+    function draw() {
+        var pos;
 
-		// Start by clearing the screen:
-		backBuffer.clearRect(0, 0, mElement.width, mElement.height);
+        // Start by clearing the screen:
+        backBuffer.clearRect(0, 0, mElement.width, mElement.height);
 
-		// Draw nodes and connections:
-		for (var i = 0; i < nodes.length; i++) {
-			pos = nodes[i].getPosition();
+        // Draw nodes and connections:
+        for (var i = 0; i < nodes.length; i++) {
+            pos = nodes[i].getPosition();
 
-			backBuffer.beginPath();
-			backBuffer.arc(pos.x, pos.y, 40, 2 * Math.PI, 0);
-			backBuffer.fillStyle = "gray";
+            backBuffer.beginPath();
+            backBuffer.arc(pos.x, pos.y, 40, 2 * Math.PI, 0);
+            backBuffer.fillStyle = "gray";
 
-			// tmp, color TBD:
-			if (selections.left == nodes[i] || selections.right == nodes[i])
-				backBuffer.fillStyle = "green";
+            // tmp, color TBD:
+            if (selections.left == nodes[i] || selections.right == nodes[i])
+                backBuffer.fillStyle = "green";
 
-			backBuffer.fill();
-			backBuffer.lineWidth = 5;
-			backBuffer.strokeStyle = "black";
+            backBuffer.fill();
+            backBuffer.lineWidth = 5;
+            backBuffer.strokeStyle = "black";
 
-			// tmp, color TBD:
-			if (selections.left == nodes[i] || selections.right == nodes[i])
-				backBuffer.strokeStyle = "#003300";
-			
-			backBuffer.stroke();
-		}
-	}
+            // tmp, color TBD:
+            if (selections.left == nodes[i] || selections.right == nodes[i])
+                backBuffer.strokeStyle = "#003300";
+            
+            backBuffer.stroke();
+        }
+    }
 
-	function update() {
-		if (redrawBuffer) {
-			draw();
-		}
+    function update() {
+        if (redrawBuffer) {
+            draw();
+        }
 
-		// if scrolling, shift scrolled version of back buffer onto front:
-		// --- scroll not tested in demo ---
-		if (redrawBuffer) { // || scrolling) ?
-			mainBuffer.drawImage(bElement, scroll.x, scroll.y);
-			redrawBuffer = false;
-		}
-	}
+        // if scrolling, shift scrolled version of back buffer onto front:
+        // --- scroll not tested in demo ---
+        if (redrawBuffer) { // || scrolling) ?
+            mainBuffer.drawImage(bElement, scroll.x, scroll.y);
+            redrawBuffer = false;
+        }
+    }
 
-	function scroll(xVelocity, yVelocity) {
-		// not final nor essential to demo:
-		scroll.x += xVelocity;
-		scroll.y += yVelocity;
-	}
+    function scroll(xVelocity, yVelocity) {
+        // not final nor essential to demo:
+        scroll.x += xVelocity;
+        scroll.y += yVelocity;
+    }
 
-	function resetCamera() {
-		// Not sure if actually needed.
-		scroll.x = 0;
-		scroll.y = 0;
-	}
+    function resetCamera() {
+        // Not sure if actually needed.
+        scroll.x = 0;
+        scroll.y = 0;
+    }
 
-	// Sets the width & height of the canvas:
-	function resize(width, height) {
-		mainBuffer.canvas.width  = width;
-		mainBuffer.canvas.height = height;
+    // Sets the width & height of the canvas:
+    function resize(width, height) {
+        mainBuffer.canvas.width  = width;
+        mainBuffer.canvas.height = height;
 
-		backBuffer.canvas.width  = width;
-		backBuffer.canvas.height = height;
+        backBuffer.canvas.width  = width;
+        backBuffer.canvas.height = height;
 
-		redrawBuffer = true;
-	}
+        redrawBuffer = true;
+    }
 
-	// This function will return the left and right selected nodes.
-	function getSelections() {
-		return selections;
-	}
+    // This function will return the left and right selected nodes.
+    function getSelections() {
+        return selections;
+    }
 
-	// This function will handle updating the canvas and fps syncing:
-	function animate() {
-		var time = 0,
-			inc = 0.1;
+    // This function will handle updating the canvas and fps syncing:
+    function animate() {
+        var time = 0,
+            inc = 0.1;
 
-		// Immediate Anonymous Loop:
-		(function loop() {
-			requestAnimationFrame(loop);
-			time += inc;
-			update();
-		}());
-	}
+        // Immediate Anonymous Loop:
+        (function loop() {
+            requestAnimationFrame(loop);
+            time += inc;
+            update();
+        }());
+    }
 
-	// Handle the canvas being clicked:
-	$('#' + id).mousedown(function(event) {
-		// x & y based on code from https://stackoverflow.com/questions/3067691/html5-canvas-click-event:
-		var x = event.pageX - $('#' + id).offset().left;
-		var y = event.pageY - $('#' + id).offset().top;
+    // Handle the canvas being clicked:
+    $('#' + id).mousedown(function(event) {
+        // x & y based on code from https://stackoverflow.com/questions/3067691/html5-canvas-click-event:
+        var x = event.pageX - $('#' + id).offset().left;
+        var y = event.pageY - $('#' + id).offset().top;
 
-		var pos, pythag;
+        var pos, pythag;
 
-		// Create a node if placed in open space:
-		for (var i = 0; i < nodes.length; i++) {
-			pos = nodes[i].getPosition();
+        // Create a node if placed in open space:
+        for (var i = 0; i < nodes.length; i++) {
+            pos = nodes[i].getPosition();
 
-			// Ignore node if offscreen:
-			if (pos.x < scroll.x || pos.y < scroll.y ||
-				pos.x > mainBuffer.canvas.width + scroll.x ||
-				pos.y > mainBuffer.canvas.height + scroll.y) {
-				
-				continue;
-			}
-			
-			pythag = Math.pow((x - pos.x), 2) + Math.pow((y - pos.y), 2); // x^2 + y^2:
-			// Node was selected.
-			if (pythag < 1600) { // radius of 40^2 = 1600
-				switch (event.which) {
-					case 1: // Left mouse
-						if (selections.left == nodes[i]) { // For future: Do not deselect if the detail panel is checked to stay open
-							//$("#leftDetail").hide();
-							$(window).resize();
-							selections.left = null;
-							redrawBuffer = true;
-						}
-						else {
-							//$("#leftDetail").show();
-							$(window).resize();
-							selections.left = nodes[i];
-							redrawBuffer = true;
-						}
-						return;
-					case 2: // Middle mouse
+            // Ignore node if offscreen:
+            if (pos.x < scroll.x || pos.y < scroll.y ||
+                pos.x > mainBuffer.canvas.width + scroll.x ||
+                pos.y > mainBuffer.canvas.height + scroll.y) {
+                
+                continue;
+            }
+            
+            pythag = Math.pow((x - pos.x), 2) + Math.pow((y - pos.y), 2); // x^2 + y^2:
+            // Node was selected.
+            if (pythag < 1600) { // radius of 40^2 = 1600
+                switch (event.which) {
+                    case 1: // Left mouse
+                        if (selections.left == nodes[i]) { // For future: Do not deselect if the detail panel is checked to stay open
+                            //$("#leftDetail").hide();
+                            $(window).resize();
+                            selections.left = null;
+                            redrawBuffer = true;
+                        }
+                        else {
+                            //$("#leftDetail").show();
+                            $(window).resize();
+                            selections.left = nodes[i];
+                            redrawBuffer = true;
+                        }
+                        return;
+                    case 2: // Middle mouse
 
-					case 3: // Right Mouse
-						return;
-					default:
-						break;
-				}
-			}
-			// Tried placing a new node onto an existing node:
-			else if (pythag < 7056) // (2*radius of 80 + small offset)^2 = 7056
-				return;
-		}
-		switch (event.which) {
-			case 3: // Right mouse
-				var node = Node(x + scroll.x, y + scroll.y);
-				nodes.push(node);
-				redrawBuffer = true;
+                    case 3: // Right Mouse
+                        return;
+                    default:
+                        break;
+                }
+            }
+            // Tried placing a new node onto an existing node:
+            else if (pythag < 7056) // (2*radius of 80 + small offset)^2 = 7056
+                return;
+        }
+        switch (event.which) {
+            case 3: // Right mouse
+                var node = Node(x + scroll.x, y + scroll.y);
+                nodes.push(node);
+                redrawBuffer = true;
 
-			default:
-				break;
-		}
-	});
+            default:
+                break;
+        }
+    });
 
-	// Here is the returned JSOL which allows public access of certain functions:
-	return {
-		animate: animate,
-		getSelections: getSelections,
-		resize: resize
-		// more stuff
-	};
+    // Here is the returned JSOL which allows public access of certain functions:
+    return {
+        animate: animate,
+        getSelections: getSelections,
+        resize: resize
+        // more stuff
+    };
 }
--- js/roots.js
+++ js/roots.js
@@ -1,46 +1,47 @@
 /*
-	Created on  : Feb 18 2014
-	Author      : Daniel Kolsoi
-	Description : Main script for roots web app.
+    Created on  : Feb 18 2014
+    Author      : Daniel Kolsoi
+    Description : Main script for roots web app.
 */
 
 $(document).ready(function(){
-	//$("#leftDetail").hide();
-	//$("#rightDetail").hide();
+    //$("#leftDetail").hide();
+    //$("#rightDetail").hide();
 
 
-	// Instantiate the canvas "class":
-	var canvas = Canvas("canvas");
-	canvas.animate();
+    // Instantiate the canvas "class":
+    var canvasWorkspace = Workspace("workspace");
+    canvasWorkspace.animate();
 
-	// Disable right click:
-	$("#canvas").bind("contextmenu", function(e) {
-		return false;
-	});
+    // Disable right click:
+    $("#workspace").bind("contextmenu", function(e) {
+        return false;
+    });
 
-	// Resize the canvas initially, and do the same upon window resize:
-	$(window).resize(function() {
-		var width = window.innerWidth;
-		var height = window.innerHeight - $("#topDiv").height();
+    // Add an event handler to resize the canvas workspace on window resize:
+    $(window).resize(function() {
+        var width = window.innerWidth;
+        var height = window.innerHeight - $("#topDiv").height();
 
-		if ($("#leftDetail").css("display") != "none") {
-			width -= $("#leftDetail").width();
-		}
+        if ($("#leftDetail").css("display") != "none") {
+            width -= $("#leftDetail").width();
+        }
 
-		if ($("#rightDetail").css("display") != "none") {
-			width -= $("#rightDetail").width();
-		}
+        if ($("#rightDetail").css("display") != "none") {
+            width -= $("#rightDetail").width();
+        }
 
-		canvas.resize(width, height);
-	});
-	$(window).resize();
+        canvasWorkspace.resize(width, height);
+    });
+    // Resize the window on document load.
+    $(window).resize();
 
-	/*
-		For Andy:
-		1. You can get the JSOL containing the left (and right) nodes from canvas.getSelections()
-			However we're only working on the left detail panel right now.
-		2. Once you have a node, you can call node.getData() to return its associated person class.
-			Be sure to figure out if JavaSript uses deep or shallow copy by default.
-	*/
+    /*
+        For Andy:
+        1. You can get the JSOL containing the left (and right) nodes from canvas.getSelections()
+            However we're only working on the left detail panel right now.
+        2. Once you have a node, you can call node.getData() to return its associated person class.
+            Be sure to figure out if JavaSript uses deep or shallow copy by default.
+    */
 
 });
