fba2e608509132fdabc8ba50f3faada889079aa1
Andrew Lincoln <andrewlincoln11@gmail.com>
2014-04-26 00:02:39 -0400

    Copy to JSON folder - Need to test on server
    Not working on OS X, but platform differences might be causing this.

--- base.php
+++ base.php
@@ -1,4 +1,5 @@
 <?php
+
 // From http://code.tutsplus.com/tutorials/user-membership-with-php--net-1523
 // Grabs session information for use with user management
 
@@ -6,14 +7,13 @@ session_start();
 
 
 /*           Database Information      */
-$dbhost = "localhost"; 
-$dbname = "alincoln"; 
-$dbuser = "alincoln"; 
+$dbhost = "localhost";
+$dbname = "alincoln";
+$dbuser = "alincoln";
 $dbpass = "al3445";
 
 mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
 mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
-
 
 /**
  * Take the username as argument (username is set in roots.php upon login)
@@ -21,10 +21,23 @@ mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
  * Create the file and grab the data from the post
  */
 function save($username) {
-    $filename =  $username . ".json";
+
+    $filename = $username . ".json";
+    $tmp = "/tmp/";                         // Absolute path to temp directory
+    $dest = dirname(__FILE__) . "/json/";   // Destination folder absolute path
+
     $tree = $_POST['tree'];
-    file_put_contents("/tmp/" . $filename, $tree);
-    //shell_exec("cp $tmp $filename json/"); TODO: Get this working
+    
+    fopen( $tmp . $filename, 'w');              // Create file in tmp with write permissions
+    file_put_contents($tmp . $filename, $tree); //Add the JSON from the POST
+    chown($tmp . $filename, get_current_user());// Change permissions to the running user NOTE: Not working on OS X
+    chmod($tmp . $filename, 0755);              //Change the permissions to be web-friendly
+
+//    $command = "cp " . $filename . " " . $dest;
+    //exec($command);
+    exec("cp " . $tmp . $filename . " " .  $dest . $filename); // Copy
+
+//    shell_exec("cp " . ("/tmp/" . $filename) . " json/");  // TODO: Get this working
 }
 
 ?>
