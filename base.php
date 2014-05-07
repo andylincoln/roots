cbc4b1d49113051ceee4dc32b56338b651732386
Andrew Lincoln <andrewlincoln11@gmail.com>
2014-05-06 23:42:32 -0400

    Deleted unnecessary code

--- base.php
+++ base.php
@@ -25,19 +25,18 @@ function save($username) {
     $filename = $username . ".json";
     $tmp = "/tmp/";                         // Absolute path to temp directory
     $dest = dirname(__FILE__) . "/json/";   // Destination folder absolute path
-
     $tree = $_POST['tree'];
 
     fopen($tmp . $filename, 'w');              // Create file in tmp with write permissions
     file_put_contents($tmp . $filename, $tree); //Add the JSON from the POST
-//    chown($tmp . $filename, get_current_user());// Change permissions to the running user NOTE: Not working on OS X
+
     chmod($tmp . $filename, 0755);              //Change the permissions to be web-friendly
-    // Three different ways to do the copy. Hopefully one of these can work
-//    $command = "cp " . $tmp . $filename . " " . $dest;
-//    echo $command;
-//    exec($command);
+
     exec("cp " . $tmp . $filename . " " . $dest . $filename); // Copy
-    shell_exec("cp " . ("/tmp/" . $filename) . " json/");  // TODO: Get this working
+    
+    if (file_exists($tmp . $filename)) {
+        exec("rm " . $tmp . $filename); // Remove temp
+    }
 }
 
 ?>
